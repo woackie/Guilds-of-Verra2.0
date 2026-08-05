c1932fb3e41826937f838b3841531de4f8ef1c35
woackie <138411037+woackie@users.noreply.github.com>
2026-08-05 11:41:07 +0200

    Test active passive summary filtering

--- src/test/java/com/guildsofverra/journal/JournalSummaryServiceTest.java
+++ src/test/java/com/guildsofverra/journal/JournalSummaryServiceTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 class JournalSummaryServiceTest {
     @Test
-    void aggregatesOnlyPurchasedNumericBonusesForTheRequestedSkill() {
+    void aggregatesOnlyPurchasedImplementedBonusesForTheRequestedSkill() {
         SkillTreeDefinition tree = new SkillTreeDefinition(
             SkillId.MINING,
             100,
@@ -20,7 +20,7 @@ class JournalSummaryServiceTest {
             List.of(
                 node("ore_one", "ore_xp", 0.10),
                 node("ore_two", "ore_xp", 0.15),
-                node("fortune", "drop_chance", 0.05),
+                node("future_fortune", "drop_chance", 0.05),
                 new SkillNodeDefinition(
                     "informational",
                     1,
@@ -38,7 +38,7 @@ class JournalSummaryServiceTest {
             Set.of(
                 "mining:ore_one",
                 "mining:ore_two",
-                "mining:fortune",
+                "mining:future_fortune",
                 "mining:informational",
                 "combat:ore_one"
             ),
@@ -48,8 +48,41 @@ class JournalSummaryServiceTest {
         );
 
         assertEquals(
-            Map.of("ore_xp", 0.25, "drop_chance", 0.05),
+            Map.of("ore_xp", 0.25),
             JournalSummaryService.passiveBonuses(profile, SkillId.MINING, tree)
+        );
+    }
+
+    @Test
+    void includesImplementedFishingAndCookingBonusTypes() {
+        SkillTreeDefinition fishing = new SkillTreeDefinition(
+            SkillId.FISHING,
+            100,
+            1,
+            List.of(node("extra", "extra_fish", 0.10))
+        );
+        SkillTreeDefinition cooking = new SkillTreeDefinition(
+            SkillId.COOKING,
+            100,
+            1,
+            List.of(node("extra", "extra_cooked_output", 0.20))
+        );
+        PlayerProfile profile = new PlayerProfile(
+            PlayerProfile.SCHEMA_VERSION,
+            Map.of(),
+            Set.of("fishing:extra", "cooking:extra"),
+            Set.of(),
+            Set.of(),
+            ""
+        );
+
+        assertEquals(
+            Map.of("extra_fish", 0.10),
+            JournalSummaryService.passiveBonuses(profile, SkillId.FISHING, fishing)
+        );
+        assertEquals(
+            Map.of("extra_cooked_output", 0.20),
+            JournalSummaryService.passiveBonuses(profile, SkillId.COOKING, cooking)
         );
     }
 
